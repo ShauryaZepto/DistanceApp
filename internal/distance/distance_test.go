95338ef26820416f96abcb812e82261c893f178e
Shaurya Priyadarshi <shaurya@prakhars-mac.local>
2024-05-20 04:02:11 +0530

    Refactor test spec of distance to take point as arguments

--- internal/distance/distance_test.go
+++ internal/distance/distance_test.go
@@ -6,7 +6,9 @@ import (
 
 func Test_DistanceBetweenPoints(t *testing.T) {
 	t.Run("Should give the distance between two point given 4 input values as coordinates", func(t *testing.T) {
-		got := DistanceBetweenPoints(0, 0, 3, 4)
+		point1 := point{0, 0}
+		point2 := point{3, 4}
+		got := DistanceBetweenPoints(point1, point2)
 		expected := float64(5)
 		if got != expected {
 			t.Errorf("Expected %f got %f", expected, got)
